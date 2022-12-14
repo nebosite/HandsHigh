7e34fd42973c1714a0c58ac4d9d290974ea7d59a
Eric Jorgensen <ericjorg@thejcrew.net>
2022-12-14 08:30:48 -0800

    Clean up formatting

---
+++ src/pages/FlashMob.tsx
@@ -0,0 +1,79 @@
+import HandsHighAssets from "assets/Assets";
+import { MediaHelper } from "helpers/MediaHelper";
+import { observer } from "mobx-react";
+import { ClientModel } from "models/ClientModel";
+import React from "react";
+
+
+
+interface FlashMobProps {
+    model?: ClientModel
+}
+
+// -------------------------------------------------------------------
+// LobbyComponent
+// -------------------------------------------------------------------
+@observer
+export class FlashMob extends React.Component<FlashMobProps> {
+    private _urlParams: URLSearchParams = new URLSearchParams(window.location.search);
+    media: MediaHelper;
+
+    // -------------------------------------------------------------------
+    // ctor
+    // -------------------------------------------------------------------
+    constructor(props: FlashMobProps) {
+        super(props);
+
+        this.media = new MediaHelper();
+        for(let soundName in HandsHighAssets.sounds)
+        {
+            this.media.loadSound((HandsHighAssets.sounds as any)[soundName]);
+        }
+
+        if(props.model) {
+            props.model.playSound = (name, semiTone, volume) => {
+                const sound = (HandsHighAssets.sounds as any)[name];
+                if(sound) {
+                    const rateAdjust = this.media.soundHelper.getRateAdjust(semiTone)
+                    this.media.playSound(sound, {volume, rateAdjust});
+                }
+            }            
+        }
+    }
+
+    //--------------------------------------------------------------------------------------
+    // 
+    //--------------------------------------------------------------------------------------
+    componentDidMount(): void {
+        const label = document.getElementById("FastPrinter")  
+        const background = document.getElementById("ThePage")
+        const colors= ["#a0a0a0", "#a8a8a8", "#afafaf", "#a8a8a8"]
+        let colorIndex = 0;
+
+        setInterval(()=>{
+            if(label) {
+                label.innerText = `Seconds to start: ${this.props.model?.secondsToStart}`
+            }
+            if(background) {
+                background.style.backgroundColor = colors[colorIndex++ % 4]
+            }
+        },200)
+    }
+
+    // -------------------------------------------------------------------
+    // render
+    // -------------------------------------------------------------------
+    render() {
+        const {model} = this.props;
+        if(!model) {
+            return <div>No Model?</div>
+        }
+
+        return <div id="ThePage" style={{height: "100%"}}>
+            <h2>MAIN PAGE</h2>
+            <div id="FastPrinter" />
+            <button onClick={()=>model.start()}>Click to start</button>
+        </div>
+    };
+
+}
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,7 @@
-import HandsHighAssets from "assets/Assets";
-import { MediaHelper } from "helpers/MediaHelper";
 import { observer } from "mobx-react";
 import { ClientModel } from "models/ClientModel";
 import React from "react";
-
-
+import styles from './MainPage.module.css';
 
 interface MainPageProps {
     model?: ClientModel
@@ -15,50 +12,6 @@ interface MainPageProps {
 // -------------------------------------------------------------------
 @observer
 export class MainPage extends React.Component<MainPageProps> {
-    private _urlParams: URLSearchParams = new URLSearchParams(window.location.search);
-    media: MediaHelper;
-
-    // -------------------------------------------------------------------
-    // ctor
-    // -------------------------------------------------------------------
-    constructor(props: MainPageProps) {
-        super(props);
-
-        this.media = new MediaHelper();
-        for(let soundName in HandsHighAssets.sounds)
-        {
-            this.media.loadSound((HandsHighAssets.sounds as any)[soundName]);
-        }
-
-        if(props.model) {
-            props.model.playSound = (name, semiTone, volume) => {
-                const sound = (HandsHighAssets.sounds as any)[name];
-                if(sound) {
-                    const rateAdjust = this.media.soundHelper.getRateAdjust(semiTone)
-                    this.media.playSound(sound, {volume, rateAdjust});
-                }
-            }            
-        }
-    }
-
-    //--------------------------------------------------------------------------------------
-    // 
-    //--------------------------------------------------------------------------------------
-    componentDidMount(): void {
-        const label = document.getElementById("FastPrinter")  
-        const background = document.getElementById("ThePage")
-        const colors= ["#a0a0a0", "#a8a8a8", "#afafaf", "#a8a8a8"]
-        let colorIndex = 0;
-
-        setInterval(()=>{
-            if(label) {
-                label.innerText = `Seconds to start: ${this.props.model?.secondsToStart}`
-            }
-            if(background) {
-                background.style.backgroundColor = colors[colorIndex++ % 4]
-            }
-        },200)
-    }
 
     // -------------------------------------------------------------------
     // render
@@ -69,11 +22,12 @@ export class MainPage extends React.Component<MainPageProps> {
             return <div>No Model?</div>
         }
 
-        return <div id="ThePage" style={{height: "100%"}}>
-            <h2>MAIN PAGE</h2>
-            <div id="FastPrinter" />
-            <button onClick={()=>model.start()}>Click to start</button>
+        return <div className={styles.MainPage}> 
+            <h2>Hands High!</h2>
+            <div>
+                <h4>Flash Mob</h4>
+            </div>
         </div>
     };
 
-}
+} 
